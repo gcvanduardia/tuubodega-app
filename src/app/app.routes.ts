936d83ae2909f3d6fa1f950ada3ff9974a322412
David Suarez <gcvanguardia@gmail.com>
2024-02-14 08:09:47 -0500

    feat: fix landing out

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full', },
+  { path: '', redirectTo: 'home', pathMatch: 'full', },
   { path: 'home', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
   { path: 'home/:search', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
-  { path: 'product-description/:id', loadComponent: () => import('./product-description/product-description.page').then( m => m.ProductDescriptionPage) },
-  { path: '', loadComponent: () => import('./landing/landing.page').then( m => m.LandingPage) },
+  { path: 'product-description/:id', loadComponent: () => import('./product-description/product-description.page').then( m => m.ProductDescriptionPage) }
 ];

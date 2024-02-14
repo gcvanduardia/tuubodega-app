fea398460e40bd3e488cc5d24bbe8d6c793789dc
David Suarez <gcvanguardia@gmail.com>
2024-02-14 08:28:31 -0500

    feat: fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full', },
+  { path: '', redirectTo: '', pathMatch: 'full', },
+  { path: '', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
   { path: 'home', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
   { path: 'home/:search', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
+  { path: 'search/:search', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
   { path: 'product-description/:id', loadComponent: () => import('./product-description/product-description.page').then( m => m.ProductDescriptionPage) }
 ];
